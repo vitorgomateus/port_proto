314d6161078b2255d060e79ce9e9744c6b2a2c93
vitorgomateus <vitorg8.5@gmail.com>
2018-03-14 00:03:55 +0000

    hm

--- portproto.js
+++ portproto.js
@@ -1,8 +1,26 @@
+var jqueryMousewheel = require("jquery-mousewheel");
 
-const LoadingAnimation = require('loading-animation');
+document.addEventListener('scroll', function (event) {
+    //if (event.target.id === 'idOfUl') { // apply any filtering condition        
+    //    console.log('scroll event', event.target);
+    //}
+
+}, true /*Capture event*/);
+
+$('body').mousewheel(function(event) {
+    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+    alert("hey");//noup
+});
+
+$(document).ready(function(){
+			
+		}
+	)
+
+/*const LoadingAnimation = require('loading-animation');
 let anim = new LoadingAnimation('Text before'); // animation starts automatically
 setTimeout(()=>{
-    anim.stop('Ok');
+	anim.stop('Ok');
 }, 3000);
 
-anim = new LoadingAnimation(['Text before', 'and text after'], 'green')
+anim = new LoadingAnimation(['Text before', 'and text after'], 'green')*/
